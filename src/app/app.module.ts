e41784a267654d7fc81e5c459d2ae33326eb7444
braian-soto <sotobraian18@hotmail.com>
2022-09-29 23:45:28 -0300

    segunda entrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { ContenidoComponent } from './components/contenido/contenido.component';
-
+import { DirectivasComponent } from './components/directivas/directivas.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    ToolbarComponent,
-    ContenidoComponent
+    DirectivasComponent,
   ],
   imports: [
     BrowserModule
